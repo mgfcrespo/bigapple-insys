256eb5ab480acbab31d390bccf45f813208e5eaf
Ian Eugenio <ipreugenio12321@gmail.com>
2018-05-30 11:28:43 +0800

    partial models

--- bigapple/inventory/models.py
+++ bigapple/inventory/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField('name', max_length=200)
+    address = models.CharField('address', max_length=200)
+    contact_number = models.CharField('contact_number', max_length=45)
+
+class MaterialRequisition(models.Model):
+    SHIFTS = (
+        ('A', 'shift 1'),
+        ('B', 'shift 2'),
+        ('C', 'shift 3')
+    )
+    date_issued = models.DateField('date_issued')
+    issued_to = models.CharField('issued_to', max_length=200)
+    brand = models.CharField('brand', max_length=200)
+    description = models.CharField('description', max_length=200)
+    quantity = models.IntegerField('quantity')
+    to_be_used_for = models.CharField('to_be_used_for', max_length=200)
+    shift = models.CharField('shift', choices=SHIFTS, default='not specified')
+    approval = models.BooleanField('approval', default=false)
+
+class PurchaseRequisition(models.Model):
+    #placed_by = models.ForeignKey(Accounts.name)
+    #department = models.ForeignKey(Accounts.department)
+    date_issued = models.DateField('date_issued')
+    date_required = models.DateField('date_required')
+    #supplier may not be specified on request-- supplier = models.ForeignKey(Supplier, on_delete = models.CASCADE)
+    description = models.CharField('description', max_length=200)
+    quantity = models.IntegerField('quantity')
+    approval = models.BooleanField('approval', default=false)
+
+class Inventory(models.Model):
+    rm_type = models.CharField('rm_type', max_length=200)
+    description = models.CharField('descrption', max_length=200)
+    brand = models.CharField('brand', max_length=200)
+    quantity = models.IntegerField('quantity')
+
+
--- bigapple/sales/models.py
+++ bigapple/sales/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField('name', max_length=200)
+    address = models.CharField('address', max_length=200)
+    contact_number = models.CharField('contact_number', max_length=45)
+    email = models.CharField('email', max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class CustomerItem(models.Model):
+    item_type = models.CharField('item_type', max_length=200)
+    description = models.CharField('description', max_length=200)
+    width = models.IntegerField('width')
+    length = models.IntegerField('length')
+    color = models.CharField('color',  max_length=200)
+    gusset = models.CharField('gusset',  max_length=200)
+    quantity = models.IntegerField('quantity')
+    price = models.IntegerField('price')
+    total_amount = models.IntegerField('total_amount')
+    # sample_layout = models.CharField('sample_layout', max_length=200)
+
+    def __str__(self):
+        return self.item_type
+
+class CustomerPO(models.Model):
+    #name = models.CharField('name', max_length=200)
+    #address = models.CharField('address', max_length=200) redundant?
+    date_issued = models.DateField('date_issued')
+    date_required = models.DateField('date_required')
+    terms = models.CharField('name', max_length=250)
+    other_info = models.CharField('other_info', max_length=250)
+    customers = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer_items = models.ManyToManyField(CustomerItem)
+
+    '''
+    def po_number(self):
+        return 'PO%s' % (self.)
+    '''
