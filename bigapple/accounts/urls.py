4dcba34beadf13b7560da1520c44f4018c3f0f43
mikee1897 <33421122+mikee1897@users.noreply.github.com>
2018-06-06 13:42:45 +0800

    account template

--- bigapple/accounts/urls.py
+++ bigapple/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.conf.urls import include
+from django.conf.urls import include, url
 from . import views
 
 app_name='accounts'
 urlpatterns = [
-
-        ]
+        url(r'^accountDetails/', views.accountDetails, name='accountDetails')
+];
--- bigapple/accounts/views.py
+++ bigapple/accounts/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Account
+
 # Create your views here.
+def accountDetails(request):
+    #return HttpResponse('HELLO FROM POSTS')
+
+    context = {
+        'title': 'Accounts'
+    }
+
+    return render(request, 'accounts/accountDetails.html', context)
