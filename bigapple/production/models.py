4eb23645e628e697df1260b95fb33709235da392
replicantdeca <iana_decastro@dlsu.edu.ph>
2018-06-10 16:18:39 +0800

    updated models

--- bigapple/production/models.py
+++ bigapple/production/models.py
@@ -39,23 +39,46 @@ class SalesInvoice(models.Model):
 class MachineSchedule(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
 
-class CuttingSchedule(models.Model):
-    
-    
 class PrintingSchedule(models.Model):
+
 '''
+
+class CuttingSchedule(models.Model):
+    client_po = models.ForeignKey(ClientPO, on_delete=models.CASCADE)
+    print_name = models.CharField('print_name')
+    sealing = models.CharField('sealing')
+    handle = models.CharField('handle')
+    operator = models.CharField('operator', max_length=200)
+    date = models.DateField('date')
+    time_in = models.TimeField('time_in')
+    time_out = models.TimeField('time_out')
+    line = models.IntegerField('line', min_length=1)
+    quantity = models.FloatField('quantity')
+    kilos = models.FloatField('kilos')
+    number_rolls = models.FloatField('number_rolls')
+    starting_scrap = models.FloatField('starting_scrap')
+    cutting_scrap = models.FloatField('cutting_scrap')
+    remarks = models.CharField('remarks', max_length=1000)
+
 
 class ExtruderSchedule(models.Model):
     client_po = models.ForeignKey(ClientPO, on_delete=models.CASCADE)
     jo_number = models.IntegerField('jo_number', min_length=6)
     stock_kind = models.CharField('stock_kind', max_length=250)
-    kilos = models.FloatField('kilos')
+    material = models.CharField('material', max_length=200)
     treating = models.CharField('treating', max_length=200);
     operator = models.CharField('operator', max_length=200)
     date = models.DateField('date')
+    time_in = models.TimeField('time_in')
+    time_out = models.TimeField('time_out')
+    kilos = models.FloatField('kilos')
     number_rolls = models.FloatField('number_rolls')
     weight_rolls = models.FloatField('weight_rolls')
-    #core_weight, net_weight, starting_scrap, extruder_scrap, balance, remarks
+    core_weight = models.FloatField('core_weight')
+    net_weight = models.FloatField('net_weight') # idk if necessary
+    starting_scrap = models.FloatField('starting_scrap')
+    extruder_scrap = models.FloatField('extruder_scrap')
+    remarks = models.CharField('remarks', max_length=1000)
 
 
 
